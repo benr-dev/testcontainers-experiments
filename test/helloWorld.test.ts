aa68967259ed1b290673e7563dcdf82a7bd73997
benr-dev <54710382+benr-dev@users.noreply.github.com>
2022-07-23 22:02:49 +0100

    feat: add testcontainers and associated test

--- test/helloWorld.test.ts
+++ test/helloWorld.test.ts
@@ -1,5 +1,23 @@
-import { helloWorld } from '../src/helloWorld';
+import { GenericContainer, StartedTestContainer } from "testcontainers";
+import axios from "axios";
 
-test('hello world tests', () => {
-    expect(helloWorld()).toBe('Hello World');
-})
+export const container = new GenericContainer(
+  "nginxdemos/hello"
+).withExposedPorts(80);
+
+export let runningContainer: StartedTestContainer;
+
+beforeAll(async () => {
+  runningContainer = await container.start();
+}, 15000);
+
+afterAll(async () => {
+  await runningContainer.stop();
+});
+
+test("hello world tests", async () => {
+  const containerPort = runningContainer.getMappedPort(80);
+  const result = await axios.get(`http://localhost:${containerPort}`);
+
+  expect(result.data).toContain("Hello World");
+});
