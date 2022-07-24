907517f376db6a0aa9363d9e5ee6151417b25603
benr-dev <54710382+benr-dev@users.noreply.github.com>
2022-07-24 22:22:23 +0100

    chore: documentation

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # testcontainers-experiments
 
 Some experiments using testcontainers for unit tests in a typescript project.
+
+## Basic configuration for an integration test
+- Host needs a docker runtime
+- module needs testcontainers as a dev dependency
+- test suite creates a new container wrapper, initialised with the image and version name
+- test suite should start the container in a beforeAll function within the suite
+- test suite should shutdown the container in an afterAll function within the suite
+- a random host port will be allocated for each exposed container port. These need to be looked up before use
+
--- test/helloWorld.test.ts
+++ test/helloWorld.test.ts
@@ -7,17 +7,20 @@ export const container = new GenericContainer(
 
 export let runningContainer: StartedTestContainer;
 
-beforeAll(async () => {
-  runningContainer = await container.start();
-}, 15000);
+describe("unit tests using a hello-world container", () => {
+  beforeAll(async () => {
+    runningContainer = await container.start();
+  }, 30000);  // Long timeout as container startup is not immediate
 
-afterAll(async () => {
-  await runningContainer.stop();
-});
+  afterAll(async () => {
+    await runningContainer.stop();
+  });
 
-test("hello world tests", async () => {
-  const containerPort = runningContainer.getMappedPort(80);
-  const result = await axios.get(`http://localhost:${containerPort}`);
+  test("should be able to get 'Hello World' page from the container", async () => {
+    // Find the host port which testcontainers has allocated to the exposed port inside the container
+    const containerPort = runningContainer.getMappedPort(80);  
+    const result = await axios.get(`http://localhost:${containerPort}`);
 
-  expect(result.data).toContain("Hello World");
+    expect(result.data).toContain("Hello World");
+  });
 });
