b9ed8aaef55fad4d01918cb1c83b32763aa9f24f
benr-dev <54710382+benr-dev@users.noreply.github.com>
2023-01-22 22:52:36 +0000

    feat: increase timeout to container startup

--- bdd/steps/given.ts
+++ bdd/steps/given.ts
@@ -1,7 +1,7 @@
 import { Given } from '@cucumber/cucumber';
 import { GenericContainer } from 'testcontainers';
 
-Given('the Hello World Service is running', async function() {
+Given('the Hello World Service is running', {timeout: 10000}, async function() {
      this.container = new GenericContainer(
         "nginxdemos/hello"
       ).withExposedPorts(80);
